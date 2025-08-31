626574723820a7f95f17efc18beff067b49ee476
Cong Khai <dangcongkhai2k5@gmail.com>
2025-08-31 11:18:32 +0700

    Update project setting

--- mcp_server.py
+++ mcp_server.py
@@ -1,24 +1,36 @@
 from mcp.server.fastmcp import FastMCP
 
 
-mcp = FastMCP(name = "mcp_server")
+import logging
 
+mcp = FastMCP(name="mcp_server")
 
 
 @mcp.tool(description="Get weather tool")
 def get_weather(location: str):
+    logging.info("Get weather")
     return 20
 
 
 # Define resource
 @mcp.resource(uri="file://{name}.txt")
-def get_hello_file(name: str):
-    """Get a person name"""
-    return f"Hello, {name}"
+def get_info(name: str):
+    """Get a person info"""
+    logging.info("Get person info")
+    return f"Hello, {name}. Here is your resource"
 
 
 @mcp.prompt()
 def find_weather():
-    prompt = "Give me the weather condition in a specified place in the most specific way"
+    logging.info("Return user prompt")
+    prompt = (
+        "Give me the weather condition in a specified place in the most specific way"
+    )
+    return prompt
+
 
 # Set up a server with necessary tool and resourc
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(mcp.run(transport="sse"))
