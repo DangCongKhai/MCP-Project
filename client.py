4cdb3840c560a5a745e3cbfbe49eb1d4c140112f
Cong Khai <dangcongkhai2k5@gmail.com>
2025-09-10 23:34:29 +0700

    Update client parameters

--- client.py
+++ client.py
@@ -2,23 +2,30 @@ import asyncio
 import os
 
 
-from mcp import ClientSession, StdioServerParameters
+from mcp import ClientSession, StdioServerParameters,
 from mcp.client.stdio import stdio_client
 from mcp.types import PromptReference, ResourceTemplateReference
 
 
-
 class MCPCLient:
-    def __init__(self):
+    def __init__(self, name, transport, config):
+        self.name = name
+        self.transport = transport
+        self.config = config
+        self.client
+
+    async def initialize_connnection(self, server_script_path):
+
+        if self.transport == 'stdio':
+            self.server_params = StdioServerParameters(
+                command="python", args=[server_script_path]
+            )
+        # elif 'http' in self.transport:
+        #     self.server_params = 
+        
+        # Next step: set up connection and store it
+
+    async def chat_loop(self):
         pass
-    
-    
-    def initialize_connnection(self):
+    async def clean_up(self):
         pass
-    
-    
-    def chat_loop(self):
-        pass
-    def clean_up(self):
-        pass
-    
