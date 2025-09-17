e56864a62d69c9925e3248443b5cccde245b40dc
Cong Khai <dangcongkhai2k5@gmail.com>
2025-09-17 22:33:33 +0700

    Setting up main.py for testing

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import streamlit as st
+# import streamlit as st
 
-def main():
-    st.write("hello world. This is my application")
+# def main():
+#     st.write("hello world. This is my application")
+
+
+import asyncio
+from client import MCPClient
+
+
+async def main():
+
+    # Initialize client and connect it to the server
+    client = MCPClient()
+    try:
+        await client.connnect_to_server("mcp_server.py")
+        
+        await client.start_chat_loop()
+
+    except Exception as e:
+        print(e)
+    finally:
+        await client.cleanup()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
